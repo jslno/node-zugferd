397d5395fe2b54a8ac40366f704ffb3552ab6371
Joel Solano <solano.joel@gmx.de>
2025-02-18 21:47:37 +0100

    fix: linting

--- docs/src/lib/constants.ts
+++ docs/src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const ENV = {
-  NEXT_PUBLIC_WEBSITE_URL:
-    process.env.NEXT_PUBLIC_WEBSITE_URL || "http://localhost:3000",
-  NODE_ENV: process.env.NODE_ENV || "development",
+	NEXT_PUBLIC_WEBSITE_URL:
+		process.env.NEXT_PUBLIC_WEBSITE_URL || "http://localhost:3000",
+	NODE_ENV: process.env.NODE_ENV || "development",
 };
