d37698abab49090a13c3bc226763ba38b414c664
Joel Solano <joelsolano@jsolano.de>
2025-03-13 16:53:43 +0100

    feat: improved metadata

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,20 +1,31 @@
 import { NavBar } from "@/components/nav-bar";
 import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
-import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { NavProvider } from "@/components/nav-mobile";
+import { createMetadata } from "@/lib/metadata";
 
-const inter = Inter({
-	subsets: ["latin"],
+export const metadata = createMetadata({
+	title: {
+		template: "%s | node-zugferd",
+		default: "node-zugferd",
+	},
+	description: "Integrate seamless electronic invoicing in Node.js",
+	metadataBase:
+		process.env.NODE_ENV === "development"
+			? new URL("http://localhost:3000")
+			: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`),
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
-		<html lang="en" className={inter.className} suppressHydrationWarning>
+		<html lang="en" suppressHydrationWarning>
+			<head>
+				<link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+			</head>
 			<body
 				className={`${GeistSans.variable} ${GeistMono.variable} font-sans relative`}
 			>
