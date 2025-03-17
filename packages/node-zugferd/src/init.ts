bf6f508c697fd7449507ab2a59ae620b420ec7b0
Joel Solano <joelsolano@jsolano.de>
2025-03-17 15:38:21 +0100

    refactor: improve formatters file structure (#24)
    * refactor: improve formatters file structure
    
    * chore: fix linting

--- packages/node-zugferd/src/init.ts
+++ packages/node-zugferd/src/init.ts
@@ -1,4 +1,8 @@
-import { formatXml, mergeSchemas, parseSchema } from "./xml-formatter";
+import {
+	formatXml,
+	mergeSchemas,
+	parseSchema,
+} from "./formatter/xml/formatter";
 import {
 	addPdfMetadata,
 	addPdfTrailerInfoId,
@@ -6,7 +10,7 @@ import {
 	addPdfStructTreeRoot,
 	addPdfMarkInfo,
 	addPdfICC,
-} from "./pdf-formatter";
+} from "./formatter/pdf";
 import { type ZugferdOptions } from "./types/options";
 
 export const init = (options: ZugferdOptions) => {
