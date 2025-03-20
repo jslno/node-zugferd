16e14a90f785a3d1eded6208df976887d1290aa6
Joel Solano <solano.joel@gmx.de>
2025-03-20 04:26:54 +0100

    chore: fix linting

--- packages/node-zugferd/src/utils/asset.ts
+++ packages/node-zugferd/src/utils/asset.ts
@@ -2,8 +2,8 @@ import { createRequire } from "module";
 import path from "path";
 
 export const getAsset = (_path: string) => {
-	const require = createRequire(import.meta.url)
-	const x = require.resolve("node-zugferd")
+	const require = createRequire(import.meta.url);
+	const x = require.resolve("node-zugferd");
 
 	return path.join(path.dirname(x), _path);
 };
