f4e5348ecca46c946095b29ed9aa2801fe785521
Joel Solano <solano.joel@gmx.de>
2025-03-20 03:06:34 +0100

    fix: resolve xsd path error

--- packages/node-zugferd/src/profiles/basic-wl/index.ts
+++ packages/node-zugferd/src/profiles/basic-wl/index.ts
@@ -2,12 +2,13 @@ import { createProfile } from "../factory";
 import { type InferSchema } from "../../types/schema";
 import { MINIMUM } from "../minimum";
 import { basicWlSchema } from "./schema";
+import { getAsset } from "../../utils/asset";
 
 export const BASIC_WL = createProfile({
 	contextParameter: "urn:factur-x.eu:1p0:basicwl",
 	extends: [MINIMUM],
 	schema: basicWlSchema,
-	xsdPath: "Factur-X_1.07.2_BASICWL.xsd",
+	xsdPath: getAsset("Factur-X_1.07.2_BASICWL.xsd"),
 	conformanceLevel: "BASIC WL",
 	documentFileName: "factur-x.xml",
 	documentType: "INVOICE",
--- packages/node-zugferd/src/profiles/basic/index.ts
+++ packages/node-zugferd/src/profiles/basic/index.ts
@@ -2,13 +2,14 @@ import { createProfile } from "../factory";
 import { type InferSchema } from "../../types/schema";
 import { BASIC_WL } from "../basic-wl";
 import { basicSchema } from "./schema";
+import { getAsset } from "../../utils/asset";
 
 export const BASIC = createProfile({
 	contextParameter:
 		"urn:cen.eu:en16931:2017#compliant#urn:factur-x.eu:1p0:basic",
 	extends: [...BASIC_WL.extends, BASIC_WL],
 	schema: basicSchema,
-	xsdPath: "Factur-X_1.07.2_BASIC.xsd",
+	xsdPath: getAsset("Factur-X_1.07.2_BASIC.xsd"),
 	conformanceLevel: "BASIC",
 	documentFileName: "factur-x.xml",
 	documentType: "INVOICE",
