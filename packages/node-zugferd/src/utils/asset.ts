ed6bcf3f3c1d3656eb4e553bb1774aa5a456fb0f
bastiion <github@sebastian-tilsch.de>
2025-06-27 13:25:59 +0200

    fix: make getAsset function ESM and CommonJS friendly (#77)
    * fix: make getAsset function ESM and CommonJS friendly
    
    * chore: fix linting
    
    ---------
    
    Co-authored-by: Joel Solano <joelsolano@jsolano.de>

--- packages/node-zugferd/src/utils/asset.ts
+++ packages/node-zugferd/src/utils/asset.ts
@@ -2,10 +2,24 @@ import { createRequire } from "module";
 import path from "path";
 
 export const getAsset = (_path: string) => {
-	let req =
-		typeof import.meta !== "undefined"
-			? createRequire(import.meta.url)
-			: require;
+	let req: NodeJS.Require;
+	try {
+		if (typeof import.meta !== "undefined" && import.meta.url) {
+			req = createRequire(import.meta.url);
+		} else {
+			req = require;
+		}
+	} catch (error) {
+		req = require;
+	}
 
-	return path.join(path.dirname(req.resolve("node-zugferd")), _path);
+	try {
+		const resolvedPath = req.resolve("node-zugferd");
+		const dirname = path.dirname(resolvedPath);
+		const fullPath = path.join(dirname, _path);
+
+		return fullPath;
+	} catch (error) {
+		throw error;
+	}
 };
